a4d991241978d95edec09e288fde218f38b381a8
Alejandro Muñoz <alejandro_mr92@hotmail.com>
2018-12-09 22:57:02 +0100

    Moved missing files to Kotlin language

--- app/src/main/java/alejandromr92/com/movies/network/Constants.kt
+++ app/src/main/java/alejandromr92/com/movies/network/Constants.kt
@@ -2,13 +2,11 @@ package alejandromr92.com.movies.network
 
 interface Constants {
     companion object {
-        val READ_TIME_OUT = 60L
-        val WRITE_TIME_OUT = 60L
-        val CONNECT_TIME_OUT = 60L
+        const val READ_TIME_OUT = 60L
+        const val WRITE_TIME_OUT = 60L
+        const val CONNECT_TIME_OUT = 60L
 
-        val NETWORK_ERROR = 404
-
-        val API_KEY = "api_key"
-        val API_PAGE = "page"
+        const val API_KEY = "api_key"
+        const val API_PAGE = "page"
     }
 }
